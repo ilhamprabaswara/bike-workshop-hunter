e9a142cdb0c675299f2f99bee71ae24535f9a893
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2024-02-24 19:33:09 +0700

    feat: Add raleway font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { raleway } from "@/assets/fonts";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html className={`${raleway.variable} px-5 py-10`} lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   );
