b7b77faaef21f57903909e3f88e9cb978ce2fa49
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2024-02-28 22:54:17 +0700

    feat(cms): Create dashboard listings and reviews page UI

--- src/app/(cms-site)/dashboard/listings/page.tsx
+++ src/app/(cms-site)/dashboard/listings/page.tsx
@@ -1,33 +1,14 @@
-import { Payment, columns } from "@/components/cms-site/dashboard/columns";
+"use client";
+import { columns } from "@/components/cms-site/dashboard/columns";
 import { DataTable } from "@/components/cms-site/dashboard/data-table";
+import { useGetMasterData } from "@/lib/hooks/dashboard/hooks";
 
-async function getData(): Promise<Payment[]> {
-  // Fetch data from your API here.
-  return [
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    {
-      id: "728sd52f",
-      amount: 100,
-      status: "success",
-      email: "a@example.com",
-    },
-    {
-      id: "728sd5sf",
-      amount: 100,
-      status: "pending",
-      email: "z@example.com",
-    },
-  ];
-}
-
-export default async function DemoPage() {
-  const data = await getData();
-
+export default function DashboardPage() {
+  // const data = await getData();
+  const { data, isFetching } = useGetMasterData();
+  if (isFetching) {
+    return <h1>Loading</h1>;
+  }
   return (
     <div className="container mx-auto">
       <DataTable columns={columns} data={data} />
