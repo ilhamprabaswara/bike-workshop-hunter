126a692576de3e1922d83b83b7caeb28b0056388
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2024-02-28 22:53:00 +0700

    feat(cms): Create login page UI

--- src/app/(cms-site)/(authentication)/login/page.tsx
+++ src/app/(cms-site)/(authentication)/login/page.tsx
@@ -1,5 +1,48 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 const LoginPage = () => {
-  return <h1>Login</h1>;
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="max-w-[500px]">
+        <Card>
+          <CardHeader className="space-y-1">
+            <CardTitle className="text-2xl">Welcome</CardTitle>
+            <CardDescription>
+              Enter your email to sign in to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" placeholder="m@example.com" />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">Sign In</Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
