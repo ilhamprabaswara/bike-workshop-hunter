b4b75d6ccba28fc98d67611eae6cbce3965d1e75
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2024-02-28 22:55:18 +0700

    feat(cms): Create edit workshop page

---
+++ src/app/(cms-site)/dashboard/workshop/edit/[workshopId]/page.tsx
@@ -0,0 +1,91 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Switch } from "@/components/ui/switch";
+import { useForm } from "react-hook-form";
+
+const DashboardWorkshopEditPage = () => {
+  const form = useForm({
+    // resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      location: "",
+    },
+  });
+
+  function onSubmit(values: any) {
+    console.log(values);
+  }
+  return (
+    <div className="px-5">
+      <h1>Edit Workshop</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Workshop Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>This is your workshop name.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="location"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Location</FormLabel>
+                <FormControl>
+                  <Input placeholder="Jakarta" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="contact"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Contact</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Contact" {...field} />
+                  </FormControl>
+                </FormItem>
+              );
+            }}
+          />
+          <FormField
+            name="status"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Workshop Status</FormLabel>
+                <FormControl>
+                  <Switch
+                    className="flex"
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+export default DashboardWorkshopEditPage;
