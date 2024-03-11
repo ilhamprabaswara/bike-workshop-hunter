08798d8380827c1761f7729b95ef61f3bf3e969f
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2024-03-11 19:17:35 +0700

    feat(cms): Create header for dashboard app

--- src/app/(cms-site)/dashboard/layout.tsx
+++ src/app/(cms-site)/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import { DashboardNav } from "@/components/cms-site/layout/sidebar";
 import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 interface DashboardLayoutProps {
@@ -36,6 +37,16 @@ export default async function DashboardLayout({
   }
   return (
     <>
+      <header className="bg-background sticky top-0 z-40 border-b">
+        <div className="container flex h-16 items-center justify-between py-4">
+          <div className="flex gap-6 md:gap-10">
+            <Link href="/" className="hidden items-center space-x-2 md:flex">
+              <span className="hidden font-bold sm:inline-block">BWHunter</span>
+            </Link>
+          </div>
+          <div>{data.user.user_metadata.first_name}</div>
+        </div>
+      </header>
       <div className="flex min-h-screen flex-col space-y-6">
         <div className="container mt-10 grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
           <aside className="hidden w-[200px] flex-col md:flex">
